73c2d3285b4d74709e9a6a2829e5d0574276de24
mrabbadester <antos2lite@gmail.com>
2023-02-28 13:06:45 -0300

    Primeiro commit

---
+++ src/Transformese_Serasa/soma.java
@@ -0,0 +1,18 @@
+package Transformese_Serasa;
+
+public class soma {
+
+public static void main(String[] args) {
+		
+		double media, nota1, nota2;
+		
+		nota1 = 8;
+		nota2 = 7;
+		
+		media = nota1 + nota2;
+		
+		System.out.println("media é");
+        System.out.println(media);
+	}
+
+}
